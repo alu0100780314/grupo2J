f97438c113b31ca3281028f4ee4a6e1c2d1cd513
Ivan Trujillo <alu0100780314@ull.edu.es>
2013-04-29 17:26:37 +0100

    Se ha creado el documento python sobre la integracion de Simpson y se han reorganizado los archivos

---
+++ src/python/Integracion-Simpson.py
@@ -0,0 +1,18 @@
+#####################################################################
+#
+# Titulo: Integracion numerica por Simpson
+#
+# Autores: Ivan Trujillo Trujillo
+#          Samuel Santos Lucas Castilla
+#
+# Grupo:  2J
+#
+######################################################################
+
+#!/usr/bin/python
+
+from math import *
+
+
+
+if __name__ == '__main__':
