ae3cd5f2a53250c8297aed3a2f4247efc01f6ff0
Ivan Trujillo <alu0100780314@ull.edu.es>
2013-05-03 16:44:37 +0100

    Se ha terminado el módulo de integración por Simpson.

--- src/python/Integracion-Simpson.py
+++ src/python/Integracion-Simpson.py
@@ -13,14 +13,25 @@
 
 from math import *
 
-def evalua_expresion(x):
+def ev_funcion(x):
   return 1/(1+exp(x))
+  
+def ev_funcion_int(x):
+  return log(exp(x))-log(exp(x)+1)
+  
+def int_real(a,b):
+  return ev_funcion_int(b)-ev_funcion_int(a)
 
-def simpson(a,b):
-  return ((b-a)/6)*(evalua_expresion(a)+4*evalua_expresion((a+b)/2)+evalua_expresion(b))
+def int_simpson(a,b):
+  return ((b-a)/6)*(ev_funcion(a)+4*ev_funcion((a+b)/2)+ev_funcion(b))
 
 if __name__ == '__main__':
-  expr='1/(1+exp(x))'
+  funcion='1/(1+exp(x))'
+  funcion_int='log(exp(x))-log(exp(x)+1)'
   a=1.0
   b=6.0
-  print ("La integral definida entre %.1f y %.1f de %s es aproximadamente %.10f." % (a,b,expr,simpson(a,b)))
+  error=abs(int_real(a,b)-int_simpson(a,b))
+  print ("La integral definida entre %.1f y %.1f de %s es realmente %.7f unidades cuadradas." % (a,b,funcion,int_real(a,b)))
+  print ("La integral definida entre %.1f y %.1f de %s es aproximadamente %.7f unidades cuadradas." % (a,b,funcion,int_simpson(a,b)))
+  print ("El error absoluto entre los dos calculos es %.10f." % error)
+
